58b2c4bbf71e2d5308bcd629fec8a9a60bf8432c
alexsak <a.sakakushev@gmail.com>
2020-12-25 01:14:31 +0100

    Adding Navbar

--- mindor/src/App.js
+++ mindor/src/App.js
@@ -1,48 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter } from 'react-router-dom';
 import { MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 
     return (
-      <>
-      <BrowserRouter>
-
-        <MDBNav className="justify-content-center">
-          <MDBNavItem>
-            <MDBNavLink active to="#!">Active</MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink to="#!">Link</MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink to="#!">Link</MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink disabled to="#!">Disabled</MDBNavLink>
-          </MDBNavItem>
-        </MDBNav>
-
-      </BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code>!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Don't Learn React
-          </a>
-        </header>
+          <Navbar />
       </div>
-      </>
   );
 }
 
